ff8137906a24d3270d56a45ce7bd9e9fbea8b01c
John Sweeney <jes2806@gmail.com>
2022-09-19 15:53:48 -0600

    calculating different values in table (exercise 5)

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,16 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum("annual_revenue")
+
+@average_revenue = @total_revenue / Store.count
+
+@million_plus_stores = Store.where("annual_revenue > 1000000")
+
+@million_plus_stores_count = @million_plus_stores.count
+
+puts @total_revenue
+puts @average_revenue
+puts @million_plus_stores_count
+
